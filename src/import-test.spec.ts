81e297522d5bdd87397456cd7458b1efe79485b3
Antti Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-12-12 10:14:11 +0200

    added empty tests

---
+++ src/import-test.spec.ts
@@ -0,0 +1,25 @@
+import 'mocha';
+
+describe('Save three blocks', () => {
+
+  it('should contain three blocks', () => {
+    //expect("Hello world!").to.equal('Hello world!');
+  });
+
+  it('should contain 7 addresses', () => {
+    //expect("Hello world!").to.equal('Hello world!');
+  });
+
+  it('should contain 2 address clusters', () => {
+    //expect("Hello world!").to.equal('Hello world!');
+  });
+
+  it('first cluster should have a balance of 100', () => {
+    //expect("Hello world!").to.equal('Hello world!');
+  });
+
+  it('second cluster should have a balance of 50', () => {
+    //expect("Hello world!").to.equal('Hello world!');
+  });
+
+});
